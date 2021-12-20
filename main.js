f42e961b837da54dd6251d79fe30bc2f79ed2e90
mahirscode <mahirscode@gmail.com>
2021-12-20 14:00:10 +0300

    SignUp Commited

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const button = document.querySelector('button');
+
+
+
+button.addEventListener('click', () => {
+    alert(`Error Required Field!`)
+})
